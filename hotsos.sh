96730f5c5ab07451aa78d65a36741e3d19bd0299
Edward Hope-Morley <edward.hope-morley@canonical.com>
2020-06-02 20:48:08 +0100

    Modularise

--- hotsos.sh
+++ hotsos.sh
@@ -19,9 +19,10 @@
 #  Generate a high-level summary of a sosreport.
 #
 # Authors:
-#  - edward.hope-morley@canonical.com
-#  - opentastic@gmail.com
+#${indent_str}edward.hope-morley@canonical.com
+#${indent_str}opentastic@gmail.com
 
+indent_str="  - "
 declare -a sos_paths=()
 write_to_file=false
 
@@ -103,6 +104,13 @@ while (($#)); do
     shift
 done
 
+unit_in_array ()
+{
+    unit="$1"
+    shift
+    echo $@| egrep -q "\s?${unit}\s?"
+}
+
 ((${#sos_paths[@]})) || { usage; exit 1; }
 ((${#sos_paths[@]})) || sos_paths=( . )
 
@@ -116,7 +124,7 @@ if ${PLUGINS[all]}; then
 fi
 
 f_output=`mktemp`
-
+CWD=`dirname $0`
 for root in ${sos_paths[@]}; do
 (
 
@@ -136,241 +144,11 @@ for root in ${sos_paths[@]}; do
 
 [ -z "$root" ] || cd $root
 
-echo -e "## sosreport-summary ##\n" > $f_output
-
-echo -e "hostname:\n  - `cat hostname`" >> $f_output
-if ${PLUGINS[versions]}; then
-    echo -e "versions:" >> $f_output
-    echo -n "  - ubuntu: " >> $f_output
-    sed -r 's/DISTRIB_CODENAME=(.+)/\1/g;t;d' etc/lsb-release >> $f_output
-
-    echo -n "  - openstack: " >> $f_output
-    apts='etc/apt/sources.list.d/*.list'
-    if [ -d "`dirname \"$apts\"`" ] && `grep -qr ubuntu-cloud.archive $apts 2>/dev/null`; then
-        ost_rel="`grep -r ubuntu-cloud.archive $apts| grep -v deb-src |\
-            sed -r 's/.+-updates\/(.+)\s+.+/\1/g;t;d'`"
-        [ -n "$ost_rel" ] || ost_rel=unknown
-        echo "$ost_rel" >> $f_output
-    else
-        echo "distro" >> $f_output
-    fi
-fi
-
-if ${PLUGINS[openstack]}; then
-    echo -e "openstack:" >> $f_output
-
-    # TODO: keep this list up-to-date with services we care about in the context of openstack
-    default_pfix_match='[[:alnum:]\-]+'
-    declare -a services=(
-    aodh$default_pfix_match
-    apache$default_pfix_match
-    barbican$default_pfix_match
-    beam.smp
-    ceilometer$default_pfix_match
-    ceph-[[:alpha:]]+
-    cinder$default_pfix_match
-    designate$default_pfix_match
-    glance$default_pfix_match
-    gnocchi$default_pfix_match
-    heat$default_pfix_match
-    horizon
-    keystone$default_pfix_match
-    mysqld
-    neutron$default_pfix_match
-    nova$default_pfix_match
-    octavia$default_pfix_match
-    openstack-dashboard
-    rabbitmq-server
-    rados$default_pfix_match
-    swift$default_pfix_match
-    vault$default_pfix_match
-    qemu-system-[[:alnum:]\\-\\_]+
-    )
-    if [ -r "ps" ]; then
-        declare -A openstack_info=()
-        for svc in ${services[@]}; do
-            readarray -t out<<<"`sed -r -e \"s/.+($svc)\s+.+/\1/g;t;d\" ps`"
-            ((${#out[@]}==0)) || [ -z "${out[0]}" ] && continue
-            for e in ${out[@]}; do
-                n=${openstack_info[$e]:-0}
-                openstack_info[$e]=$((n+1))
-            done
-        done
-        if ((${#openstack_info[@]})); then
-            for e in ${!openstack_info[@]}; do
-                echo "  - $e (${openstack_info[$e]})"
-            done| sort -k 2 >> $f_output
-        else
-            echo "  - null" >> $f_output
-        fi
-    else
-        echo "  ps not found - skipping openstack service detection" >> $f_output
-    fi
-fi
-
-if ${PLUGINS[storage]}; then
-    echo -e "ceph:" >> $f_output
-
-    services=(
-    ceph-osd
-    ceph-mon
-    ceph-mgr
-    radosgw
-    )
-    if [ -r "ps" ]; then
-        hash=`md5sum $f_output`
-        ( for svc in ${services[@]}; do
-            out="`sed -r \"s/.*(${svc}[[:alnum:]\-]*)\s+.+/\1/g;t;d\" ps| sort| uniq| sed -r 's/^\s+/  /g'`"
-            id="`sed -r \"s/.*(${svc}[[:alnum:]\-]*)\s+.+--id\s+([[:digit:]]+)\s+.+/\2/g;t;d\" ps| tr -s '\n' ','| sort| sed -r -e 's/^\s+/  /g' -e 's/,$//g'`"
-            [ -z "$out" ] && continue
-            for osd_id in `echo $id| tr ',' ' '`;do
-                echo -n "  - ceph-osd (id=$osd_id)"
-                if [ -e "sos_commands/ceph/ceph-volume_lvm_list" ]; then
-                    offset=`egrep -n "osd id\s+$osd_id\$" sos_commands/ceph/ceph-volume_lvm_list| cut -f1 -d:`
-                    osd_fsid=`tail -n+$offset sos_commands/ceph/ceph-volume_lvm_list| grep "osd fsid"| head -n 1| sed -r 's/.+\s+([[:alnum:]]+)/\1/g'`
-                    osd_device=`tail -n+$offset sos_commands/ceph/ceph-volume_lvm_list| grep "devices"| head -n 1| sed -r 's/.+\s+([[:alnum:]\/]+)/\1/g'`
-                    echo -n " (fsid=$osd_fsid) (device=$osd_device)"
-                fi
-                echo ""
-            done
-        done ) >> $f_output
-        [ "$hash" = "`md5sum $f_output`" ] && echo "  - null" >> $f_output
-    else
-        echo "  ps not found - skipping ceph service detection" >> $f_output
-    fi
-    
-    echo "bcache-info:" >> $f_output
-    readarray -t bcacheinfo<<<"`grep . sos_commands/block/ls_-lanR_.sys.block| egrep 'bcache|nvme'| sed -r 's/.+[[:digit:]\:]+\s+([[:alnum:]]+)\s+.+/\1/g'`"
-    ((${#bcacheinfo[@]})) && [ -n "${bcacheinfo[0]}" ] || bcacheinfo=( "null" )
-    block_root=sos_commands/block/udevadm_info_.dev.
-    for bcache_name in ${bcacheinfo[@]}; do
-        [[ $bcache_name = "null" ]] && echo $bcache_name && break
-        backing_dev_fs_uuid=`sed -r 's,^S: bcache/by-uuid/([[:alnum:]\-]+).*,\1,g;t;d' sos_commands/block/udevadm_info_.dev.$bcache_name`
-        f=`grep -l ID_FS_UUID=$backing_dev_fs_uuid ./sos_commands/block/udevadm_info_.dev.*`
-        backing_dev=${f##*.}
-
-        dname=`grep ' disk/by-dname' ${block_root}$bcache_name| sed -r 's,.+/(.+),\1,g'`
-        entry=$bcache_name
-        if [ -e "${block_root}$bcache_name" ]; then
-            entry="/dev/$bcache_name"
-        fi
-
-        if [ -n "$dname" ]; then
-            entry="$entry (dname=$dname)"
-            if [ -n "$backing_dev" ]; then
-                entry="$entry (backing=/dev/$backing_dev)"
-            fi
-        fi
-
-        echo $entry
-    done| xargs -l -I{} echo "  - {}" >> $f_output
-fi
-
-unit_in_array ()
-{
-    unit="$1"
-    shift
-    echo $@| egrep -q "\s?${unit}\s?"
-}
-
-if ${PLUGINS[juju]}; then
-    if [ -d "var/log/juju" ]; then
-        echo -e "juju:" >> $f_output
-
-        readarray -t ps_units<<<"`egrep unit-\* ps| sed -r 's,.+unit-([[:alnum:]\-]+-[[:digit:]]+).*,\1,g;t;d'| sort -u`"
-        readarray -t log_units<<<"`find var/log/juju -name unit-\*| sed -r 's,.+unit-([[:alnum:]\-]+-[[:digit:]]+).*.log.*,\1,g;t;d'| sort -u`"
-        combined_units=( `echo ${ps_units[@]} ${log_units[@]}| tr -s ' ' '\n'| sort -u` )
-
-        readarray -t ps_machines<<<"`egrep machine-\* ps| sed -r 's,.+machine-([[:digit:]]+).*,\1,g;t;d'| sort -u`"
-        readarray -t log_machines<<<"`find var/log/juju -name machine-\*| sed -r 's,.+machine-([[:digit:]]+).*.log.*,\1,g;t;d'| sort -u`"
-
-        declare -a juju_machine_running=()
-        declare -a juju_machine_stopped=()
-        
-        for machine in ${log_machines[@]}; do
-            agent_conf=var/lib/juju/agents/machine-${machine}/agent.conf
-            version=unknown
-            if [ -r "$agent_conf" ]; then
-                version=`sed -r 's/upgradedToVersion:\s+(.+)/\1/g;t;d' $agent_conf`
-            fi
-            if unit_in_array $machine ${ps_machines[@]}; then
-                juju_machine_running+=( "${machine} (version=$version)\n" )
-            else
-                juju_machine_stopped+=( "${machine}\n" )
-            fi
-        done
-
-        declare -a juju_unit_local=()
-        declare -a juju_unit_local_not_running=()
-        declare -a juju_unit_nonlocal=()
-        
-        for unit in ${combined_units[@]}; do
-            if unit_in_array $unit ${log_units[@]}; then
-                if unit_in_array $unit ${ps_units[@]}; then
-                    juju_unit_local+=( "${unit}\n" )
-                else
-                    juju_unit_local_not_running+=( "${unit}\n" )
-                fi
-            else
-                juju_unit_nonlocal+=( "${unit}\n" )
-            fi
-        done
-
-        (("${#ps_machines[@]}")) && [ -n "${ps_machines[0]}" ] || ps_machines+=( null )
-        echo -e "  machines:" >> $f_output
-        echo -e "    running:" >> $f_output
-        echo -e ${juju_machine_running[@]}| sort -u| xargs -l -I{} echo "      - {}" >> $f_output
-        if ((${#juju_machine_stopped[@]})) && [ -n "${juju_machine_stopped[0]}" ]; then
-            echo -e "    stopped:" >> $f_output
-            echo -e ${juju_machine_stopped[@]}| sort -u| xargs -l -I{} echo "      - {}" >> $f_output
-        fi
-
-        echo -e "  units:" >> $f_output
-        if (("${#ps_units[@]}"==0)) || [ -z "${ps_units[0]}" ]; then
-            echo -e "    any:" >> $f_output
-            echo "        - null" >> $f_output
-        else
-            echo -e "    running:" >> $f_output
-            echo -e ${juju_unit_local[@]}| sort -u| xargs -l -I{} echo "      - {}" >> $f_output
-            echo -e "    stopped:" >> $f_output
-            echo -e ${juju_unit_local_not_running[@]}| sort -u| xargs -l -I{} echo "      - {}" >> $f_output
-            echo -e "    non-local (e.g. lxd):" >> $f_output
-            if (("${#juju_unit_nonlocal[@]}"==0)) || [ -z "${juju_unit_nonlocal[0]}" ]; then
-                juju_unit_nonlocal=( null )
-            fi
-            echo -e ${juju_unit_nonlocal[@]}| sort -u| xargs -l -I{} echo "      - {}" >> $f_output
-        fi
-    fi
-fi
-
-if ${PLUGINS[system]}; then
-echo -e "system:" >> $f_output
-sed -r 's/.+(load average:.+)/- \1/g' uptime|xargs -l -I{} echo "  {}" >> $f_output
-echo "  - rootfs: `egrep ' /$' df`" >> $f_output
-fi
-
-if ${PLUGINS[kernel]}; then
-    echo -e "kernel:" >> $f_output
-    path=proc/cmdline
-    if [ -e "$path" ]; then
-        cat $path|xargs -l -I{} echo "  - {}" >> $f_output
-    else
-        echo "  - $path not found" >> $f_output
-    fi
-
-    echo -e "systemd:" >> $f_output
-    path=sos_commands/systemd/systemctl_show_service_--all
-    if [ -e "$path" ]; then
-        if `egrep -q "CPUAffinity=.+" $path`; then
-            egrep "CPUAffinity=.+" $path| sort -u|xargs -l -I{} echo "  - {}"  >> $f_output
-        else
-            echo "  - CPUAffinity not set"  >> $f_output
-        fi
-    else
-        echo "  - null" >> $f_output
-    fi
-fi
-
+echo -e "hostname:\n${indent_str}`cat hostname`" > $f_output
+for plugin in ${!PLUGINS[@]}; do
+    [ "$plugin" = "all" ] && continue
+    ${PLUGINS[$plugin]} && . $CWD/plugins/$plugin
+done
 )
 
 if $write_to_file; then
