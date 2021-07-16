fad21fd669ff789f6bb9ce36fdde761684abf85c
Edward Hope-Morley <edward.hope-morley@canonical.com>
2021-07-16 10:59:23 +0100

    update build.sh for snapcraft 4.8.3

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ tox
 # these get created when running units tests so need to remove since they will break hotsos
 find plugins -name __pycache__ -type d| xargs -l rm -rf
 
-snapcraft clean
-snapcraft
+snapcraft clean --destructive-mode
+snapcraft --destructive-mode
 snapcraft push hotsos_1.0_amd64.snap
 
 echo "Don't forget to snapcraft release!"
