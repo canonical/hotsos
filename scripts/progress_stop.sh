28043f8166d7783794f6721134318ecf546901e3
Dan Streetman <ddstreet@ieee.org>
2022-03-31 10:04:51 -0400

    change bash progress spinner scripts to python progress spinner

--- scripts/progress_stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -u
-if [[ -r $PROGRESS_PID_PATH ]]; then
-    kill -s HUP `cat $PROGRESS_PID_PATH` &>/dev/null
-    while [[ -r /proc/`cat $PROGRESS_PID_PATH` ]]; do
-        sleep .1
-    done
-    rm $PROGRESS_PID_PATH
-fi
-
