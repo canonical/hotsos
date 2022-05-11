64e8a8bdf2043c9d902da930fce5e4ee0f45c67e
Erlon R. Cruz <erlon@canonical.com>
2022-05-11 09:16:09 -0300

    Adds checks for mysql-router bug #1971565
    Resolves: #342

--- tests/unit/test_mysql.py
+++ tests/unit/test_mysql.py
@@ -1,24 +1,28 @@
 import os
 import tempfile
 
+from unittest import mock
+
 from . import utils
 
 from hotsos.core.config import setup_config
+from hotsos.core.host_helpers.systemd import SystemdService
+from hotsos.core.issues import IssuesManager
 from hotsos.plugin_extensions.mysql import summary
 from hotsos.core.ycheck.scenarios import YScenarioChecker
 
 
 SYSTEMD_UNITS = """
-  jujud-unit-hacluster-mysql-1.service   loaded active     running   juju unit agent for hacluster-mysql/1                                    
-  jujud-unit-mysql-1.service             loaded active     running   juju unit agent for mysql/1                                              
-  mysql.service                          loaded active     running   Percona XtraDB Cluster daemon  
+  jujud-unit-hacluster-mysql-1.service   loaded active     running   juju unit agent for hacluster-mysql/1
+  jujud-unit-mysql-1.service             loaded active     running   juju unit agent for mysql/1
+  mysql.service                          loaded active     running   Percona XtraDB Cluster daemon
 """  # noqa
 
 SYSTEMD_UNIT_FILES = """
-jujud-unit-hacluster-mysql-1.service   enabled        
-jujud-unit-mysql-1.service             enabled        
-mysql.service                          enabled        
-mysql@.service                         disabled  
+jujud-unit-hacluster-mysql-1.service   enabled
+jujud-unit-mysql-1.service             enabled
+mysql.service                          enabled
+mysql@.service                         disabled
 """  # noqa
 
 DPKG_L = """
@@ -30,6 +34,10 @@ ii  mysql-client-core-5.7             5.7.36-0ubuntu0.18.04.1                   
 ii  mysql-common                      5.8+1.0.4                                      all          MySQL database common files, e.g. /etc/mysql/my.cnf
 ii  python3-mysqldb                   1.3.10-1build1                                 amd64        Python interface to MySQL
 """  # noqa
+
+DPKG_L_ROUTER = """
+ii  mysql-router                      8.0.29-0ubuntu0.21.10.1                        amd64        route connections from MySQL clients to MySQL servers
+""" # noqa
 
 
 class MySQLTestsBase(utils.BaseTestCase):
@@ -69,7 +77,29 @@ class TestMySQLSummary(MySQLTestsBase):
 
 class TestMySQLScenarios(MySQLTestsBase):
 
-    def test_all(self):
-        # This is a dummy test since there are no scenarios defined yet. Once
-        # we add a scenario we should replace this test with a real test.
+    @mock.patch(
+        'hotsos.core.host_helpers.systemd.ServiceChecksBase.services', {
+            'mysql-router':
+            SystemdService('mysql-router', 'enabled')
+        })
+    @mock.patch('hotsos.core.host_helpers.packaging.CLIHelper')
+    @mock.patch('hotsos.core.ycheck.engine.YDefsLoader._is_def',
+                new=utils.is_def_filter('mysql/bugs.yaml'))
+    def test_1971565(self, mock_helper):
+        mock_helper.return_value = mock.MagicMock()
+        mock_helper.return_value.dpkg_l.return_value = \
+            DPKG_L_ROUTER.splitlines()
+
         YScenarioChecker()()
+        expected = {
+            'bugs-detected': [
+                {'id': 'https://bugs.launchpad.net/bugs/1971565',
+                 'desc': ("Installed package 'mysql-router' with "
+                          "version 8.0.29-0ubuntu0.21.10.1 has a "
+                          "known bug that prevents mysql-router from "
+                          "starting. Please upgrade to the latest "
+                          "version to fix this issue."),
+                 'origin': 'mysql.01part'}
+            ]
+        }
+        self.assertEqual(IssuesManager().load_bugs(), expected)
