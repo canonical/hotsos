canonical/hotsos

commit 2bd9aa3e993a14187931ad549673ccec245a6058

Don't cache search results in scenario checks
Search results can be large and therefore consume lots
of memory. We dont actually need to cache them so this
stops the caching of results and adjusts any ydefs that
were referencing the cached results.

hotsos/core/ycheck/engine/properties/checks.py
@@ -18,20 +18,20 @@ from hotsos.core.ycheck.engine.properties.input import YPropertyInput
 @add_to_property_catalog
 class YPropertyCheck(YPropertyMappedOverrideBase):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._search_results = None
-
-    @property
+    @cached_yproperty_attr
     def search_results(self):
-        if self.search and self._search_results is not None:
-            return self._search_results
+        """
+        Retrieve the search results for the search property within this check.
+        """
+        global_results = self.context.search_results
+        if global_results is not None:
+            log.debug("extracting check search results")
+            tag = self.search.unique_search_tag
+            _results = global_results.find_by_tag(tag)
+            return self.search.apply_constraints(_results)
 
-        raise Exception("search results not set")
-
-    @search_results.setter
-    def search_results(self, value):
-        self._search_results = value
+        raise Exception("no search results provided to check '{}'".
+                        format(self.check_name))
 
     @classmethod
     def _override_keys(cls):
@@ -46,44 +46,40 @@ class YPropertyCheck(YPropertyMappedOverrideBase):
         if hasattr(self, 'check_name'):
             return getattr(self, 'check_name')
 
+    def _set_search_cache_info(self, results):
+        """
+        @param results: search results for query in search property found in
+                        this check.
+        """
+        self.search.cache.set('num_results', len(self.search_results))
+        if results:
+            # The following aggregates results by group/index and stores in
+            # the property cache to make them accessible via
+            # PropertyCacheRefResolver.
+            results_by_idx = {}
+            for result in results:
+                for idx, value in enumerate(result):
+                    if idx not in results_by_idx:
+                        results_by_idx[idx] = set()
+
+                    results_by_idx[idx].add(value)
+
+            for idx in results_by_idx:
+                self.search.cache.set('results_group_{}'.format(idx),
+                                      list(results_by_idx[idx]))
+
+        # make it available from this property
+        self.cache.set('search', self.search.cache)
+
     def _result(self):
         if self.search:
-            if self.cache.search:
-                results = self.cache.search.results
-                log.debug("check %s - using cached result=%s", self.name,
-                          results)
-            else:
-                results = self.search_results
-                # save raw first
-                self.search.cache.set('results_raw', results)
-                # now save actual i.e. with constraints applied
-                results = self.search.apply_constraints(results)
-                self.search.cache.set('results', results)
-
-                # The following aggregates results by group/index and stores in
-                # the property cache to make them accessible via
-                # PropertyCacheRefResolver.
-                results_by_idx = {}
-                for result in results:
-                    for idx, value in enumerate(result):
-                        if idx not in results_by_idx:
-                            results_by_idx[idx] = set()
-
-                        results_by_idx[idx].add(value)
-
-                for idx in results_by_idx:
-                    self.search.cache.set('results_group_{}'.format(idx),
-                                          list(results_by_idx[idx]))
-
-                self.cache.set('search', self.search.cache)
-
-            if not results:
+            self._set_search_cache_info(self.search_results)
+            if not self.search_results:
                 log.debug("check %s search has no matches so result=False",
                           self.name)
                 return False
 
             return True
-
         elif self.requires:
             if self.cache.requires:
                 result = self.cache.requires.passes
@@ -126,23 +122,16 @@ class YPropertyChecks(YPropertyOverrideBase):
         log.debug("loading checks searchdefs into filesearcher")
         s = FileSearcher()
         # first load all the search definitions into the searcher
-        with_searches = []
         for c in self._checks:
             if c.search:
-                with_searches.append(c)
                 # local takes precedence over global
                 _input = c.input or input
                 for path in _input.paths:
                     c.search.load_searcher(s, path)
 
+        # provide results to each check object using global context
         log.debug("executing check searches")
-        results = s.search()
-
-        # now extract each checks's set of results
-        log.debug("extracting check search results")
-        for c in with_searches:
-            tag = c.search.unique_search_tag
-            c.search_results = results.find_by_tag(tag)
+        YDefsContext.search_results = s.search()
 
     @cached_yproperty_attr
     def _checks(self):

defs/scenarios/kernel/network.yaml
@@ -14,7 +14,7 @@ conclusions:
         {numreports} reports of 'nf_conntrack: table full' detected in
         kern.log - please check.
       format-dict:
-        numreports: '@checks.has_nf_conntrack_full.search.results:len'
+        numreports: '@checks.has_nf_conntrack_full.search.num_results'
   over-mtu-dropped-packets:
     decision: has_over_mtu_dropped_packets
     raises:

defs/scenarios/openvswitch/ovn/bfd_flapping.yaml
@@ -42,7 +42,7 @@ conclusions:
         resulted from BFD state changes. This could indicate that some operator
         activity is causing significant load in ovn which may or may not be expected.
       format-dict:
-        count: '@checks.chassis_gw_port_reassignments.search.results:len'
+        count: '@checks.chassis_gw_port_reassignments.search.num_results'
   bfd_is_flapping:
     priority: 2
     decision: bfd_state_changes
@@ -54,7 +54,7 @@ conclusions:
         something is wrong with the network between this node and one or more peer chassis
         nodes.
       format-dict:
-        count: '@checks.bfd_state_changes.search.results:len'
+        count: '@checks.bfd_state_changes.search.num_results'
   vswitchd_to_ovn_controller_inactivity_timeouts:
     priority: 3
     decision:
@@ -68,6 +68,6 @@ conclusions:
         timeouts between ovn-controller and ovs-vswitchd which suggests that the BFD issues are
         caused by the former failing to retrive BFD state from ovs-vswitchd.
       format-dict:
-        bfd_count: '@checks.bfd_state_changes.search.results:len'
-        timeout_count: '@checks.vswitchd_to_ovn_controller_inactivity_timeouts.search.results:len'
+        bfd_count: '@checks.bfd_state_changes.search.num_results'
+        timeout_count: '@checks.vswitchd_to_ovn_controller_inactivity_timeouts.search.num_results'
 

defs/scenarios/storage/ceph/ceph-mon/mon_elections_flapping.yaml
@@ -22,7 +22,7 @@ conclusions:
         The Ceph monitor on this host has experienced {count} re-elections within a 24hr
         period but the cause is not known - please investigate.
       format-dict:
-        count: '@checks.ceph_log_has_election_calls.search.results:len'
+        count: '@checks.ceph_log_has_election_calls.search.num_results'
   network:
     priority: 2
     decision:
@@ -37,5 +37,5 @@ conclusions:
         errors - please investigate.
       format-dict:
         interfaces: hotsos.core.plugins.storage.ceph.CephChecksBase.bind_interface_names
-        count: '@checks.ceph_log_has_election_calls.search.results:len'
+        count: '@checks.ceph_log_has_election_calls.search.num_results'
 

hotsos/core/ycheck/engine/properties/conclusions.py
@@ -225,8 +225,8 @@ class YPropertyConclusion(YPropertyMappedOverrideBase):
 
         search_results = None
         for check in checks.values():
-            if check.search and check.search.cache.results:
-                search_results = check.search.cache.results
+            if check.search and check.search_results:
+                search_results = check.search_results
                 if search_results:
                     # Save some context for the issue
                     self.issue_context.set(**{r.source: r.linenumber

tests/unit/test_ycheck.py
@@ -462,7 +462,7 @@ conclusions:
       type: SystemWarning
       message: log matched {num} times
       format-dict:
-        num: '@checks.logmatch.search.results:len'
+        num: '@checks.logmatch.search.num_results'
   logandsnap:
     priority: 2
     decision:
@@ -473,7 +473,7 @@ conclusions:
       type: SystemWarning
       message: log matched {num} times and snap exists
       format-dict:
-        num: '@checks.logmatch.search.results:len'
+        num: '@checks.logmatch.search.num_results'
   logandsnapandservice:
     priority: 3
     decision:
@@ -488,7 +488,7 @@ conclusions:
       type: SystemWarning
       message: log matched {num} times, snap and service exists
       format-dict:
-        num: '@checks.logmatch.search.results:len'
+        num: '@checks.logmatch.search.num_results'
 """  # noqa
 
 

commit 838728c38334e0e0a98b92b7b0fb47f927c20961

Fix regression introduced by commit 2bd9aa3
We were accidentally setting scenarios checks search
results on the context class rather than the context
object. This was passing in tests because we test
scenarios in isolation but in reality it was being
overwritten by successive sets of checks.

hotsos/core/searchtools.py
@@ -377,6 +377,7 @@ class FileSearcher(object):
         @param searchdef: SearchDef object
         @param path: path that we will be searching for this key
         """
+        log.debug("filesearcher: registered search with tag %s", searchdef.id)
         if path in self.paths:
             self.paths[path].append(searchdef)
         else:
@@ -615,18 +616,18 @@ class FileSearcher(object):
         self.results.reset()
         if not self.paths:
             # If no searches have been registered we don't have anything to do.
-            log.debug("no search terms registered so nothing to do")
+            log.debug("filesearcher: no search terms registered so nothing to "
+                      "do")
             return self.results
 
         num_files = self.num_files_to_search
         if not num_files:
-            log.debug("no files to search")
+            log.debug("filesearcher: no files to search")
             return self.results
 
         with multiprocessing.Pool(processes=self.num_cpus) as pool:
             jobs = {}
             for user_path in self.paths:
-                log.debug("path=%s", user_path)
                 jobs[user_path] = []
                 if os.path.isfile(user_path):
                     job = self._job_wrapper(pool, user_path, user_path)
@@ -642,7 +643,7 @@ class FileSearcher(object):
 
             num_searches = sum([len(jobs[p]) * len(self.paths[p])
                                 for p in jobs])
-            log.debug("running filesearcher processes=%d files=%d "
+            log.debug("filesearcher: running processes=%d files=%d "
                       "searches=%d", self.num_cpus, num_files, num_searches)
 
             for user_path in jobs:
@@ -654,5 +655,5 @@ class FileSearcher(object):
                     except FileSearchException as e:
                         sys.stderr.write("{}\n".format(e.msg))
 
-        log.debug("filesearcher completed (results=%s)", len(self.results))
+        log.debug("filesearcher: completed (results=%s)", len(self.results))
         return self.results

hotsos/core/ycheck/engine/properties/checks.py
@@ -25,9 +25,10 @@ class YPropertyCheck(YPropertyMappedOverrideBase):
         """
         global_results = self.context.search_results
         if global_results is not None:
-            log.debug("extracting check search results")
             tag = self.search.unique_search_tag
             _results = global_results.find_by_tag(tag)
+            log.debug("check %s has %s search results with tag %s",
+                      self.check_name, len(_results), tag)
             return self.search.apply_constraints(_results)
 
         raise Exception("no search results provided to check '{}'".
@@ -127,11 +128,12 @@ class YPropertyChecks(YPropertyOverrideBase):
                 # local takes precedence over global
                 _input = c.input or input
                 for path in _input.paths:
+                    log.debug("loading searches for check %s", c.check_name)
                     c.search.load_searcher(s, path)
 
         # provide results to each check object using global context
         log.debug("executing check searches")
-        YDefsContext.search_results = s.search()
+        self.check_context.search_results = s.search()
 
     @cached_yproperty_attr
     def _checks(self):