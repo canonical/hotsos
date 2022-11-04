bd5788883645ff1b763d1f174e1ca4acd3ab4dc3
Nicolas Bock <nicolas.bock@canonical.com>
2022-11-04 10:38:18 +0100

    DRY `requirements.txt`
    This change removes the explicit list of requirements from the `setup.py` file
    and uses the `requirements.txt` instead. This makes `requirements.txt` the
    single point of truth.
    
    The change also removes the Python `stage-packages` since they are installed via
    `requirements.txt` in the build phase.
    
    Signed-off-by: Nicolas Bock <nicolas.bock@canonical.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 from setuptools import setup, find_packages
 
+def get_dependencies():
+    """Reads the dependencies from the requirements file."""
+    with open('requirements.txt', 'r') as d:
+        dependencies = d.read()
+
+    return dependencies
+
 setup(
     name='hotsos',
     version='1.0.0',
     packages=find_packages(include=['hotsos*']),
-    install_requires=[
-        'click',
-        'pyyaml',
-        'progress',
-        'simplejson',
-        'structr',
-        'fasteners',
-        # Pin to cryptography==3.4.8 since the cryptography-3.4.x branch is the
-        # last branch that can be built without the Rust toolchain.
-        # More info on issue: https://github.com/canonical/hotsos/issues/326
-        # [TODO] Bump cryptography to more recent release
-        'cryptography==3.4.8',
-    ],
+    install_requires=get_dependencies(),
     entry_points={
       'console_scripts': [
         'hotsos=hotsos.cli:main']
