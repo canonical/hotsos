74361055aa592f378456b92dc99f7b776f664c25
Edward Hope-Morley <edward.hope-morley@canonical.com>
2023-04-24 16:11:07 +0100

    Check for conflicting installed charms
    checks added for ceph and openstack charm conflicts.
    
    Resolves: #578

---
+++ tests/unit/storage/test_ceph_common.py
@@ -0,0 +1,26 @@
+import os
+
+from .. import utils
+from hotsos.core.config import HotSOSConfig
+
+
+CEPH_MON_DATA_ROOT = os.path.join(utils.TESTS_DIR,
+                                  'fake_data_root/storage/ceph-mon')
+
+
+class CephCommonTestsBase(utils.BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        HotSOSConfig.data_root = CEPH_MON_DATA_ROOT
+        HotSOSConfig.plugin_name = 'storage'
+
+
+@utils.load_templated_tests('scenarios/storage/ceph/common')
+class TestCephCommonScenarios(CephCommonTestsBase):
+    """
+    Scenario tests can be written using YAML templates that are auto-loaded
+    into this test runner. This is the recommended way to write tests for
+    scenarios. It is however still possible to write the tests in Python if
+    required. See defs/tests/README.md for more info.
+    """
