9e0bf35ad598701eeb03d1ee97badec5a346d7bb
Mustafa Kemal Gilor <mustafa.gilor@canonical.com>
2024-04-26 16:17:04 +0300

    tests/storage/ceph-rgw: add missing ceph-rgw templated test loader
    the YAML test scenarios under tests/scenarios/storage/ceph/ceph-rgw
    were not being load due to missing test file and test loader directive.
    added `test_ceph_rgw.py` to correct that.
    
    fixed incorrect target-name for `rgw_frontend_conf.yaml`.
    
    removed a redundant line from `lp1980211_log_error_pkg_no_bug_neutron_ovn_meta.yaml`
    
    Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

---
+++ tests/unit/storage/test_ceph_rgw.py
@@ -0,0 +1,21 @@
+
+from hotsos.core.config import HotSOSConfig
+
+from .. import utils
+
+
+class StorageCephRGWTestsBase(utils.BaseTestCase):
+
+    def setUp(self):
+        super().setUp()
+        HotSOSConfig.plugin_name = 'storage'
+
+
+@utils.load_templated_tests('scenarios/storage/ceph/ceph-rgw')
+class TestCephRGWScenarios(StorageCephRGWTestsBase):
+    """
+    Scenario tests can be written using YAML templates that are auto-loaded
+    into this test runner. This is the recommended way to write tests for
+    scenarios. It is however still possible to write the tests in Python if
+    required. See defs/tests/README.md for more info.
+    """
