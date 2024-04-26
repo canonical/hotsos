514f2ddf2cf1ee6d06644cb0e57ed6825fbd5136
Edward Hope-Morley <edward.hope-morley@canonical.com>
2024-04-26 14:39:59 +0100

    Fix docrefs for writing yaml unit tests

--- tests/unit/test_pacemaker.py
+++ tests/unit/test_pacemaker.py
@@ -58,5 +58,6 @@ class TestPacemakerScenarios(TestPacemakerBase):
     Scenario tests can be written using YAML templates that are auto-loaded
     into this test runner. This is the recommended way to write tests for
     scenarios. It is however still possible to write the tests in Python if
-    required. See defs/tests/README.md for more info.
+    required. See https://hotsos.readthedocs.io/en/latest/contrib/testing.html
+    for more information.
     """
