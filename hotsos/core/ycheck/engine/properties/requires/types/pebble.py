canonical/hotsos

commit dd51c3f5565ef14decf9273423ceaf7341600736

Re-enable pylint duplicate-code check (#956)
The bulk of the changes in this patch are to the plugin_extensions
that implement summary output. Most plugins have some entries in
common and those are now moved to a base class so that they don't
need to be redefined by each plugin.

Also optimises some of the hosthelpers and checks implementations
to satisfy the duplicate-code check. Some disables have been used
where it was deemed that fixing the code would introduce too much
instability and this can be addressed in future patches.

hotsos/core/ycheck/engine/properties/requires/types/pebble.py
@@ -1,9 +1,8 @@
 from hotsos.core.log import log
 from hotsos.core.host_helpers import PebbleHelper
-from hotsos.core.ycheck.engine.properties.requires import (
-    intercept_exception,
-    ServiceCheckItemsBase,
-    YRequirementTypeBase,
+from hotsos.core.ycheck.engine.properties.requires import ServiceCheckItemsBase
+from hotsos.core.ycheck.engine.properties.requires.types import (
+    service_manager_common
 )
 
 
@@ -16,64 +15,40 @@ class PebbleServiceCheckItems(ServiceCheckItemsBase):
         return PebbleHelper(self._svcs_all)
 
 
-class YRequirementTypePebble(YRequirementTypeBase):
+class YRequirementTypePebble(service_manager_common.ServiceManagerTypeBase):
     """
     Pebble requires type property. Provides support for defining checks on
     pebble service manager resources.
     """
     override_keys = ['pebble']
     override_autoregister = True
+    default_op = 'eq'
+
+    def _check_item_settings(self, svc, svc_obj, settings, cache_info,
+                             all_items):
+        processes = None
+        if isinstance(settings, str):
+            state = settings
+            ops = [[self.default_op, state]]
+        else:
+            processes = settings.get('processes')
+            if processes:
+                log.debug("checking systemd service processes: %s", processes)
+
+            op = settings.get('op', self.default_op)
+            if 'state' in settings:
+                ops = [[op, settings.get('state')]]
+            else:
+                ops = []
+
+        if processes and not all_items.processes_running(processes):
+            log.debug("one or more pebble service processes not running "
+                      "- %s", ', '.join(processes))
+            return False
+
+        cache_info[svc]['ops'] = self.ops_to_str(ops)
+        return self.self.apply_ops(ops, input=svc_obj.state)
 
     @property
-    @intercept_exception
-    def _result(self):
-        cache_info = {}
-        default_op = 'eq'
-        _result = True
-
-        items = PebbleServiceCheckItems(self.content)
-        if not items.not_installed:
-            for svc, settings in items:
-                svc_obj = items.installed[svc]
-                cache_info[svc] = {'actual': svc_obj.state}
-                if settings is None:
-                    continue
-
-                processes = None
-                if isinstance(settings, str):
-                    state = settings
-                    ops = [[default_op, state]]
-                else:
-                    processes = settings.get('processes')
-                    if processes:
-                        log.debug("checking service processes: %s", processes)
-
-                    op = settings.get('op', default_op)
-                    if 'state' in settings:
-                        ops = [[op, settings.get('state')]]
-                    else:
-                        ops = []
-
-                if processes and not items.processes_running(processes):
-                    log.debug("one or more processes not running "
-                              "- %s", ', '.join(processes))
-                    _result = False
-                else:
-                    cache_info[svc]['ops'] = self.ops_to_str(ops)
-                    _result = self.self.apply_ops(ops, input=svc_obj.state)
-
-                if not _result:
-                    # bail on first fail
-                    break
-        else:
-            log.debug("one or more services not installed so returning False "
-                      "- %s", ', '.join(items.not_installed))
-            # bail on first fail i.e. if any not installed
-            _result = False
-
-        # this will represent what we have actually checked
-        self.cache.set('services', ', '.join(cache_info))
-        svcs = [f"{svc}={settings}" for svc, settings in items]
-        log.debug('requirement check: %s (result=%s)',
-                  ', '.join(svcs), _result)
-        return _result
+    def items_to_check(self):
+        return PebbleServiceCheckItems(self.content)

hotsos/core/ycheck/engine/properties/requires/types/service_manager_common.py
@@ -1,0 +1,56 @@
+import abc
+
+from hotsos.core.log import log
+from hotsos.core.ycheck.engine.properties.requires import (
+    intercept_exception,
+    YRequirementTypeBase,
+)
+
+
+class ServiceManagerTypeBase(YRequirementTypeBase):
+    """
+    Base class for implementations for service manager requirement types such
+    as systemd, pebble etc.
+    """
+    default_op = 'eq'
+
+    @property
+    @abc.abstractmethod
+    def items_to_check(self):
+        """ Return check items we want to iterate over until we find an error.
+
+        The implementing class should have an accompanying implementation of
+        ServiceCheckItemsBase and return that as an object here.
+        """
+
+    @property
+    @intercept_exception
+    def _result(self):
+        cache_info = {}
+        _result = True
+
+        items = self.items_to_check
+        if not items.not_installed:
+            for svc, settings in items:
+                svc_obj = items.installed[svc]
+                cache_info[svc] = {'actual': svc_obj.state}
+                if settings is None:
+                    continue
+
+                _result = self._check_item_settings(svc, svc_obj, settings,
+                                                    cache_info, items)
+                if not _result:
+                    # bail on first fail
+                    break
+        else:
+            log.debug("one or more services not installed so returning False "
+                      "- %s", ', '.join(items.not_installed))
+            # bail on first fail i.e. if any not installed
+            _result = False
+
+        # this will represent what we have actually checked
+        self.cache.set('services', ', '.join(cache_info))
+        svcs = [f"{svc}={settings}" for svc, settings in items]
+        log.debug('requirement check: %s (result=%s)',
+                  ', '.join(svcs), _result)
+        return _result

hotsos/core/ycheck/engine/properties/requires/types/systemd.py
@@ -2,10 +2,9 @@ from datetime import timedelta
 
 from hotsos.core.host_helpers import SystemdHelper
 from hotsos.core.log import log
-from hotsos.core.ycheck.engine.properties.requires import (
-    intercept_exception,
-    ServiceCheckItemsBase,
-    YRequirementTypeBase,
+from hotsos.core.ycheck.engine.properties.requires import ServiceCheckItemsBase
+from hotsos.core.ycheck.engine.properties.requires.types import (
+    service_manager_common
 )
 
 
@@ -18,13 +17,14 @@ class SystemdServiceCheckItems(ServiceCheckItemsBase):
         return SystemdHelper(self._svcs_all)
 
 
-class YRequirementTypeSystemd(YRequirementTypeBase):
+class YRequirementTypeSystemd(service_manager_common.ServiceManagerTypeBase):
     """
     Systemd requires type property. Provides support for defining checks on
     systemd resources.
     """
     override_keys = ['systemd']
     override_autoregister = True
+    default_op = 'eq'
 
     def _check_service(self, svc, ops, started_after=None):
         """
@@ -73,61 +73,37 @@ class YRequirementTypeSystemd(YRequirementTypeBase):
 
         return self.apply_ops(ops, opinput=svc.state)
 
-    @property
-    @intercept_exception
-    def _result(self):
-        cache_info = {}
-        default_op = 'eq'
-        _result = True
-
-        items = SystemdServiceCheckItems(self.content)
-        if not items.not_installed:
-            for svc, settings in items:
-                svc_obj = items.installed[svc]
-                cache_info[svc] = {'actual': svc_obj.state}
-                if settings is None:
-                    continue
-
-                processes = None
-                started_after_obj = None
-                if isinstance(settings, str):
-                    state = settings
-                    ops = [[default_op, state]]
-                else:
-                    processes = settings.get('processes')
-                    if processes:
-                        log.debug("checking service processes: %s", processes)
-
-                    op = settings.get('op', default_op)
-                    started_after = settings.get('started-after')
-                    started_after_obj = items.installed.get(started_after)
-                    if 'state' in settings:
-                        ops = [[op, settings.get('state')]]
-                    else:
-                        ops = []
-
-                if processes and not items.processes_running(processes):
-                    log.debug("one or more processes not running "
-                              "- %s", ', '.join(processes))
-                    _result = False
-                else:
-                    cache_info[svc]['ops'] = self.ops_to_str(ops)
-                    _result = self._check_service(
-                                               svc_obj, ops,
-                                               started_after=started_after_obj)
-                if not _result:
-                    # bail on first fail
-                    break
+    def _check_item_settings(self, svc, svc_obj, settings, cache_info,
+                             all_items):
+        # pylint: disable=duplicate-code
+        processes = None
+        started_after_obj = None
+        if isinstance(settings, str):
+            state = settings
+            ops = [[self.default_op, state]]
         else:
-            log.debug("one or more services not installed so returning False "
-                      "- %s", ', '.join(items.not_installed))
-            # bail on first fail i.e. if any not installed
-            _result = False
+            processes = settings.get('processes')
+            if processes:
+                log.debug("checking systemd service processes: %s", processes)
+                processes = settings.get('processes')
 
-        # this will represent what we have actually checked
-        self.cache.set('services', ', '.join(cache_info))
+            op = settings.get('op', self.default_op)
+            started_after = settings.get('started-after')
+            started_after_obj = all_items.installed.get(started_after)
+            if 'state' in settings:
+                ops = [[op, settings.get('state')]]
+            else:
+                ops = []
 
-        svcs = [f"{svc}={settings}" for svc, settings in items]
-        log.debug('requirement check: %s (result=%s)',
-                  ', '.join(svcs), _result)
-        return _result
+        if processes and not all_items.processes_running(processes):
+            log.debug("one or more systemd service processes not running "
+                      "- %s", ', '.join(processes))
+            return False
+
+        cache_info[svc]['ops'] = self.ops_to_str(ops)
+        return self._check_service(svc_obj, ops,
+                                   started_after=started_after_obj)
+
+    @property
+    def items_to_check(self):
+        return SystemdServiceCheckItems(self.content)

hotsos/plugin_extensions/openstack/agent/events.py
@@ -21,7 +21,10 @@ from hotsos.core.plugins.openstack.common import (
 from hotsos.core.plugins.openstack.neutron import NeutronHAInfo
 from hotsos.core import utils
 from hotsos.core.utils import sorted_dict
-from hotsos.core.plugintools import summary_entry
+from hotsos.core.plugintools import (
+    summary_entry,
+    get_min_available_entry_index,
+)
 
 VRRP_TRANSITION_WARN_THRESHOLD = 8
 
@@ -81,7 +84,7 @@ class ApacheEventChecks(OpenstackEventHandlerBase):
     summary_part_index = 8
     event_group = 'apache'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         out = self.run()
         if out:
@@ -110,7 +113,7 @@ class APIEvents(OpenstackEventHandlerBase):
     summary_part_index = 9
     event_group = 'http-requests'
 
-    @summary_entry('api-info', 100)
+    @summary_entry('api-info', get_min_available_entry_index() + 100)
     def summary_api_info(self):
         out = self.run()
         if out:
@@ -221,7 +224,7 @@ class NeutronAgentEventChecks(OpenstackEventHandlerBase):
     summary_part_index = 7
     event_group = 'neutron.agents'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         # NOTE: order is important here
         agents = ['neutron-server', 'neutron-l3-agent', 'neutron-ovs-agent']
@@ -280,7 +283,7 @@ class OctaviaAgentEventChecks(OpenstackEventHandlerBase):
     summary_part_index = 10
     event_group = 'octavia'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         out = self.run()
         if out:
@@ -307,7 +310,7 @@ class NovaComputeEventChecks(OpenstackEventHandlerBase):
     summary_part_index = 11
     event_group = 'nova.nova-compute'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         out = self.run()
         if out:
@@ -343,7 +346,7 @@ class AgentApparmorChecks(OpenstackEventHandlerBase):
     summary_part_index = 12
     event_group = 'apparmor'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         out = self.run()
         if out:
@@ -425,7 +428,7 @@ class NeutronL3HAEventChecks(OpenstackEventHandlerBase):
     summary_part_index = 13
     event_group = 'neutron.ml2-routers'
 
-    @summary_entry('agent-checks', 101)
+    @summary_entry('agent-checks', get_min_available_entry_index() + 101)
     def summary_agent_checks(self):
         out = self.run()
         if out:

hotsos/core/host_helpers/pebble.py
@@ -5,7 +5,6 @@ from hotsos.core.log import log
 from hotsos.core.factory import FactoryBase
 from hotsos.core.host_helpers import CLIHelper
 from hotsos.core.host_helpers.common import ServiceManagerBase
-from hotsos.core.utils import sorted_dict
 
 
 class PebbleService():
@@ -20,6 +19,10 @@ class PebbleService():
 
 class PebbleHelper(ServiceManagerBase):
     """ Helper class used to query pebble services. """
+
+    @property
+    def _service_manager_type(self):
+        return 'pebble'
 
     @cached_property
     def services(self):
@@ -38,62 +41,8 @@ class PebbleHelper(ServiceManagerBase):
         return _services
 
     @cached_property
-    def processes(self):
-        """
-        Identify running processes from ps that are associated with resolved
-        pebble services.  The search pattern used to identify a service is also
-        used to match the process binary/cmd name.
-
-        Accounts for different types of process cmd path e.g.
-
-        /snap/<name>/1830/<svc>
-        /usr/bin/<svc>
-
-        and filter e.g.
-
-        /var/lib/<svc> and /var/log/<svc>
-
-        Returns a dictionary of process names along with the number of each.
-        """
-        _proc_info = {}
-        for line in CLIHelper().ps():
-            for expr in self._service_exprs:
-                cmd = self.get_cmd_from_ps_line(line, expr)
-                if cmd:
-                    if cmd in _proc_info:
-                        _proc_info[cmd] += 1
-                    else:
-                        _proc_info[cmd] = 1
-
-        return _proc_info
-
-    @property
-    def _service_info(self):
-        """Return a dictionary of pebble services grouped by state. """
-        info = {}
-        for svc, obj in sorted_dict(self.services).items():
-            state = obj.state
-            if state not in info:
-                info[state] = []
-
-            info[state].append(svc)
-
-        return info
-
-    @property
-    def _process_info(self):
-        """Return a list of processes associated with services. """
-        return [f"{name} ({count})"
-                for name, count in sorted_dict(self.processes).items()]
-
-    @property
-    def summary(self):
-        """
-        Output a dict summary of this class i.e. services, their state and any
-        processes run by them.
-        """
-        return {'pebble': self._service_info,
-                'ps': self._process_info}
+    def _service_filtered_ps(self):
+        return CLIHelper().ps()
 
 
 class ServiceFactory(FactoryBase):

hotsos/core/host_helpers/systemd.py
@@ -19,7 +19,6 @@ from hotsos.core.factory import FactoryBase
 from hotsos.core.host_helpers import CLIHelper, CLIHelperFile
 from hotsos.core.host_helpers.common import ServiceManagerBase
 from hotsos.core.log import log
-from hotsos.core.utils import sorted_dict
 
 
 class SystemdService():
@@ -187,6 +186,10 @@ class SystemdHelper(ServiceManagerBase):
         super().__init__(*args, **kwargs)
         self._cached_unit_files_exprs = {}
 
+    @property
+    def _service_manager_type(self):
+        return 'systemd'
+
     @cached_property
     def _systemctl_list_units(self):
         return CLIHelper().systemctl_list_units()
@@ -349,66 +352,6 @@ class SystemdHelper(ServiceManagerBase):
                                 break
 
         return ps_filtered
-
-    @cached_property
-    def processes(self):
-        """
-        Identify running processes from ps that are associated with resolved
-        systemd services. The search pattern used to identify a service is also
-        used to match the process binaryc/cmd name.
-
-        Accounts for different types of process cmd path e.g.
-
-        /snap/<name>/1830/<svc>
-        /usr/bin/<svc>
-
-        and filter e.g.
-
-        /var/lib/<svc> and /var/log/<svc>
-
-        Returns a dictionary of process names along with the number of each.
-        """
-        _proc_info = {}
-        for line in self._service_filtered_ps:
-            for expr in self._service_exprs:
-                cmd = self.get_cmd_from_ps_line(line, expr)
-                if not cmd:
-                    continue
-
-                if cmd in _proc_info:
-                    _proc_info[cmd] += 1
-                else:
-                    _proc_info[cmd] = 1
-
-        return _proc_info
-
-    @property
-    def _service_info(self):
-        """Return a dictionary of systemd services grouped by state. """
-        info = {}
-        for svc, obj in sorted_dict(self.services).items():
-            state = obj.state
-            if state not in info:
-                info[state] = []
-
-            info[state].append(svc)
-
-        return info
-
-    @property
-    def _process_info(self):
-        """Return a list of processes associated with services. """
-        return [f"{name} ({count})"
-                for name, count in sorted_dict(self.processes).items()]
-
-    @property
-    def summary(self):
-        """
-        Output a dict summary of this class i.e. services, their state and any
-        processes run by them.
-        """
-        return {'systemd': self._service_info,
-                'ps': self._process_info}
 
 
 class ServiceFactory(FactoryBase):

examples/hotsos-example-juju.summary.yaml
@@ -24,13 +24,13 @@ system:
         this environment. If maintenance windows are required please consider disabling
         unattended upgrades.
 juju:
+  version: 2.9.22
   services:
     systemd:
       enabled:
         - jujud-machine-1
     ps:
       - jujud (1)
-  version: 2.9.22
   machine: '1'
   units:
     ceph-osd/0:

hotsos/core/host_helpers/common.py
@@ -4,6 +4,7 @@ import json
 import os
 import pickle
 import re
+from functools import cached_property
 
 from searchkit.utils import MPCache
 from hotsos.core.config import HotSOSConfig
@@ -12,6 +13,7 @@ from hotsos.core.host_helpers.exceptions import (
     SourceNotFound,
 )
 from hotsos.core.log import log
+from hotsos.core.utils import sorted_dict
 
 
 class NullCache():
@@ -88,6 +90,12 @@ class ServiceManagerBase(abc.ABC):
         self._ps_allow_relative = ps_allow_relative
         self._service_exprs = set(service_exprs)
 
+    @property
+    @abc.abstractmethod
+    def _service_manager_type(self):
+        """ A string name representing the type of service manager e.g.
+        'systemd' """
+
     def get_cmd_from_ps_line(self, line, expr):
         """
         Match a command in ps output line.
@@ -111,23 +119,73 @@ class ServiceManagerBase(abc.ABC):
 
     @property
     @abc.abstractmethod
+    def _service_filtered_ps(self):
+        """ Return a list ps entries corresponding to services. """
+
+    @cached_property
+    def processes(self):
+        """
+        Identify running processes from ps that are associated with resolved
+        services. The search pattern used to identify a service is also
+        used to match the process binaryc/cmd name.
+
+        Accounts for different types of process cmd path e.g.
+
+        /snap/<name>/1830/<svc>
+        /usr/bin/<svc>
+
+        and filter e.g.
+
+        /var/lib/<svc> and /var/log/<svc>
+
+        Returns a dictionary of process names along with the number of each.
+        """
+        _proc_info = {}
+        for line in self._service_filtered_ps:
+            for expr in self._service_exprs:
+                cmd = self.get_cmd_from_ps_line(line, expr)
+                if not cmd:
+                    continue
+
+                if cmd in _proc_info:
+                    _proc_info[cmd] += 1
+                else:
+                    _proc_info[cmd] = 1
+
+        return _proc_info
+
+    @property
+    @abc.abstractmethod
     def services(self):
         """ Return a dictionary of identified services and their state. """
 
     @property
-    @abc.abstractmethod
-    def processes(self):
-        """
-        Return a dictionary of processes associated with identified
-        services.
-        """
+    def _service_info(self):
+        """Return a dictionary of services grouped by state. """
+        info = {}
+        for svc, obj in sorted_dict(self.services).items():
+            state = obj.state
+            if state not in info:
+                info[state] = []
+
+            info[state].append(svc)
+
+        return info
 
     @property
-    @abc.abstractmethod
+    def _process_info(self):
+        """Return a list of processes associated with services. """
+        return [f"{name} ({count})"
+                for name, count in sorted_dict(self.processes).items()]
+
+    @property
     def summary(self):
-        """ Return a dictionary summary of this class i.e. services,
-        their state and associated processes.
         """
+        Output a dict summary of this class i.e. services, their state and any
+        processes run by them.
+        """
+        return {self._service_manager_type: self._service_info,
+                'ps': self._process_info}
 
 
 class NullSource():

commit f5d1287b4a7cac5c91ff146bcf8b0415f3ccc5bb

fix ci breakage after https://github.com/canonical/hotsos/commit/dd51c3f5565ef14decf9273423ceaf7341600736
the ci is failing due to pylint too-many-arguments and duplicate-code warnings.

it's an example for the issue described here: https://github.com/canonical/hotsos/issues/957

fixed the following:

too-many-arguments: removed svc parameter from the _check_item_settings
as it's already included in the service object.

duplicate-code: suppressed the warning.

Signed-off-by: Mustafa Kemal Gilor <mustafa.gilor@canonical.com>

hotsos/core/ycheck/engine/properties/requires/types/pebble.py
@@ -24,7 +24,7 @@ class YRequirementTypePebble(service_manager_common.ServiceManagerTypeBase):
     override_autoregister = True
     default_op = 'eq'
 
-    def _check_item_settings(self, svc, svc_obj, settings, cache_info,
+    def _check_item_settings(self, svc_obj, settings, cache_info,
                              all_items):
         processes = None
         if isinstance(settings, str):
@@ -46,8 +46,8 @@ class YRequirementTypePebble(service_manager_common.ServiceManagerTypeBase):
                       "- %s", ', '.join(processes))
             return False
 
-        cache_info[svc]['ops'] = self.ops_to_str(ops)
-        return self.self.apply_ops(ops, input=svc_obj.state)
+        cache_info[svc_obj.name]['ops'] = self.ops_to_str(ops)
+        return self.apply_ops(ops, opinput=svc_obj.state)
 
     @property
     def items_to_check(self):

hotsos/core/ycheck/engine/properties/requires/types/service_manager_common.py
@@ -37,7 +37,7 @@ class ServiceManagerTypeBase(YRequirementTypeBase):
                 if settings is None:
                     continue
 
-                _result = self._check_item_settings(svc, svc_obj, settings,
+                _result = self._check_item_settings(svc_obj, settings,
                                                     cache_info, items)
                 if not _result:
                     # bail on first fail

hotsos/core/ycheck/engine/properties/requires/types/systemd.py
@@ -73,7 +73,7 @@ class YRequirementTypeSystemd(service_manager_common.ServiceManagerTypeBase):
 
         return self.apply_ops(ops, opinput=svc.state)
 
-    def _check_item_settings(self, svc, svc_obj, settings, cache_info,
+    def _check_item_settings(self, svc_obj, settings, cache_info,
                              all_items):
         # pylint: disable=duplicate-code
         processes = None
@@ -100,7 +100,7 @@ class YRequirementTypeSystemd(service_manager_common.ServiceManagerTypeBase):
                       "- %s", ', '.join(processes))
             return False
 
-        cache_info[svc]['ops'] = self.ops_to_str(ops)
+        cache_info[svc_obj.name]['ops'] = self.ops_to_str(ops)
         return self._check_service(svc_obj, ops,
                                    started_after=started_after_obj)
 

hotsos/plugin_extensions/openstack/agent/events.py
@@ -325,6 +325,7 @@ class ApparmorCallback(OpenstackEventCallbackBase):
     event_names = ['nova', 'neutron']
 
     def __call__(self, event):
+        # pylint: disable=duplicate-code
         results = [{'date': f"{r.get(1)} {r.get(2)}",
                     'time': r.get(3),
                     'key': r.get(4)} for r in event.results]

hotsos/plugin_extensions/openvswitch/event_checks.py
@@ -54,6 +54,7 @@ class OVSEventCallbackDALR(OpenvSwitchEventCallbackBase):
     event_names = ['deferred-action-limit-reached']
 
     def __call__(self, event):
+        # pylint: disable=duplicate-code
         results = [{'date': f"{r.get(1)} {r.get(2)}",
                     'time': r.get(3),
                     'key': r.get(4)} for r in event.results]