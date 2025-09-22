93bf0b790017089dfd13e83e97fe111bd1177125
Edward Hope-Morley <edward.hope-morley@canonical.com>
2025-09-22 13:05:01 +0100

    SET-1402 Support microceph aliased ceph commands
    Provides an option to run commands with microceph
    prefix since that may sometimes be necessary.

--- hotsos/core/host_helpers/cli/commands/ceph.py
+++ hotsos/core/host_helpers/cli/commands/ceph.py
@@ -6,6 +6,9 @@ from hotsos.core.host_helpers.cli.common import BinCmd, FileCmd
 from hotsos.core.host_helpers.exceptions import SourceNotFound
 
 
+CephAliases = ['microceph.']
+
+
 class CephJSONFileCmd(FileCmd):
     """
     Some ceph commands that use --format json have some extra text added to the
@@ -58,18 +61,20 @@ class CephHealthDetailCommands(UserList):
     """ Generate ceph heath detail command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph health detail --format json-pretty',
-                       json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph health detail --format json-pretty',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/json_output/'
                             'ceph_health_detail_--format_json-pretty',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/json_output/'
-                            'ceph_health_detail_--format_json-pretty',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/json_output/'
+                             f'{prefix}ceph_health_detail_'
+                             '--format_json-pretty', json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -77,9 +82,10 @@ class CephMonDumpCommands(UserList):
     """ Generate ceph mon dump command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph mon dump --format json-pretty',
-                       json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph mon dump --format json-pretty',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(CephJSONFileCmd('sos_commands/ceph/json_output/'
@@ -88,11 +94,13 @@ class CephMonDumpCommands(UserList):
                                     first_line_filter='dumped monmap epoch',
                                     last_line_filter='dumped monmap epoch'))
         # sosreport >= 4.2
-        cmds.append(CephJSONFileCmd('sos_commands/ceph_mon/json_output/'
-                                    'ceph_mon_dump_--format_json-pretty',
-                                    json_decode=True,
-                                    first_line_filter='dumped monmap epoch',
-                                    last_line_filter='dumped monmap epoch'))
+        cmds.extend([CephJSONFileCmd('sos_commands/ceph_mon/json_output/'
+                                     f'{prefix}ceph_mon_dump_'
+                                     '--format_json-pretty',
+                                     json_decode=True,
+                                     first_line_filter='dumped monmap epoch',
+                                     last_line_filter='dumped monmap epoch')
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -100,18 +108,21 @@ class CephOSDDumpCommands(UserList):
     """ Generate ceph osd dump command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph osd dump --format json-pretty',
-                       json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph osd dump --format json-pretty',
+                       json_decode=True)
+                for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/json_output/'
                             'ceph_osd_dump_--format_json-pretty',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/json_output/'
-                            'ceph_osd_dump_--format_json-pretty',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/json_output/'
+                             f'{prefix}ceph_osd_dump_--format_json-pretty',
+                             json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -119,17 +130,20 @@ class CephDFCommands(UserList):
     """ Generate ceph df command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph df --format json-pretty', json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph df --format json-pretty',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/json_output/'
                             'ceph_df_--format_json-pretty',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/json_output/'
-                            'ceph_df_--format_json-pretty',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/json_output/'
+                             f'{prefix}ceph_df_--format_json-pretty',
+                             json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -137,18 +151,20 @@ class CepOSDDFTreeCommands(UserList):
     """ Generate ceph df tree command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph osd df tree --format json-pretty',
-                       json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph osd df tree --format json-pretty',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/json_output/'
                             'ceph_osd_df_tree_--format_json-pretty',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/json_output/'
-                            'ceph_osd_df_tree_--format_json-pretty',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/json_output/'
+                             f'{prefix}ceph_osd_df_tree_--format_json-pretty',
+                             json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -156,15 +172,19 @@ class CephOSDCrushDumpCommands(UserList):
     """ Generate ceph osd crush dump command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph osd crush dump', json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph osd crush dump',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/ceph_osd_crush_dump',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/ceph_osd_crush_dump',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/'
+                             f'{prefix}ceph_osd_crush_dump',
+                             json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -172,9 +192,11 @@ class CephPGDumpCommands(UserList):
     """ Generate ceph pg dump command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph pg dump --format json-pretty',
-                       json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph pg dump --format json-pretty',
+                       json_decode=True)
+                for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(CephJSONFileCmd('sos_commands/ceph/json_output/'
@@ -183,11 +205,13 @@ class CephPGDumpCommands(UserList):
                                     first_line_filter='dumped all',
                                     last_line_filter='dumped all'))
         # sosreport >= 4.2
-        cmds.append(CephJSONFileCmd('sos_commands/ceph_mon/json_output/'
-                                    'ceph_pg_dump_--format_json-pretty',
-                                    json_decode=True,
-                                    first_line_filter='dumped all',
-                                    last_line_filter='dumped all'))
+        cmds.extend([CephJSONFileCmd('sos_commands/ceph_mon/json_output/'
+                                     f'{prefix}ceph_pg_dump_'
+                                     '--format_json-pretty',
+                                     json_decode=True,
+                                     first_line_filter='dumped all',
+                                     last_line_filter='dumped all')
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -195,17 +219,20 @@ class CephStatusCommands(UserList):
     """ Generate ceph status command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph status --format json-pretty', json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph status --format json-pretty',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/json_output/'
                             'ceph_status_--format_json-pretty',
                             json_decode=True))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/'
-                            'json_output/ceph_status_--format_json-pretty',
-                            json_decode=True))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/json_output/'
+                             f'{prefix}ceph_status_--format_json-pretty',
+                             json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -213,13 +240,16 @@ class CephVersionsCommands(UserList):
     """ Generate ceph versions command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph versions')]
+        cmds = [BinCmd(f'{prefix}ceph versions') for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/ceph_versions'))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_mon/ceph_versions'))
+        cmds.extend([FileCmd('sos_commands/ceph_mon/'
+                             f'{prefix}ceph_versions')
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -227,13 +257,16 @@ class CephVolumeLVMListCommands(UserList):
     """ Generate ceph-volume lvm list command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph-volume lvm list')]
+        cmds = [BinCmd(f'{prefix}ceph-volume lvm list') for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(FileCmd('sos_commands/ceph/ceph-volume_lvm_list'))
         # sosreport >= 4.2
-        cmds.append(FileCmd('sos_commands/ceph_osd/ceph-volume_lvm_list'))
+        cmds.extend([FileCmd('sos_commands/ceph_osd/'
+                             f'{prefix}ceph-volume_lvm_list')
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -241,8 +274,10 @@ class CephReportCommands(UserList):
     """ Generate ceph XXX command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph report', json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph report', json_decode=True)
+                for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(CephJSONFileCmd('sos_commands/ceph/ceph_report',
@@ -250,10 +285,12 @@ class CephReportCommands(UserList):
                                     first_line_filter='report',
                                     last_line_filter='report'))
         # sosreport >= 4.2
-        cmds.append(CephJSONFileCmd('sos_commands/ceph_mon/ceph_report',
-                                    json_decode=True,
-                                    first_line_filter='report',
-                                    last_line_filter='report'))
+        cmds.extend([CephJSONFileCmd('sos_commands/ceph_mon/'
+                                     f'{prefix}ceph_report',
+                                     json_decode=True,
+                                     first_line_filter='report',
+                                     last_line_filter='report')
+                     for prefix in prefixes])
         super().__init__(cmds)
 
 
@@ -261,14 +298,17 @@ class CephMgrModuleLsCommands(UserList):
     """ Generate ceph XXX command variants. """
 
     def __init__(self):
+        prefixes = [""] + CephAliases
         # binary
-        cmds = [BinCmd('ceph mgr module ls', json_decode=True)]
+        cmds = [BinCmd(f'{prefix}ceph mgr module ls',
+                       json_decode=True) for prefix in prefixes]
         # file-based
         # sosreport < 4.2
         cmds.append(CephJSONFileCmd('sos_commands/ceph/ceph_mgr_module_ls',
                                     json_decode=True))
         # sosreport >= 4.2
-        cmds.append(CephJSONFileCmd('sos_commands/ceph_mon/'
-                                    'ceph_mgr_module_ls',
-                                    json_decode=True))
+        cmds.extend([CephJSONFileCmd('sos_commands/ceph_mon/'
+                                     f'{prefix}ceph_mgr_module_ls',
+                                     json_decode=True)
+                     for prefix in prefixes])
         super().__init__(cmds)
